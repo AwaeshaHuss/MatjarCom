69b0bab69cdf8532bc7f7b4e8e5c79168b5983f6
Hussein Al Awaesha <hussein.alawaesha@aqeltech.com>
2022-04-23 16:10:03 +0300

    handle some errors in ios folder

---
+++ lib/screens/main_acreens/home/admin_screens/home/admin_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AdminHome extends StatelessWidget {
+  const AdminHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 8, mainAxisSpacing: 8),
+      children: [],
+    );
+  }
+}
